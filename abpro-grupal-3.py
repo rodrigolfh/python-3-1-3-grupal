757c1ade5fd238f19ac6ee2331a9e4685d2834e9
felo1 <felipe.bazignan@gmail.com>
2023-04-11 12:02:23 -0400

    agregar descuento

--- abpro-grupal-3.py
+++ abpro-grupal-3.py
@@ -1,53 +1,58 @@
 #- importar copy para hacer deepcopy
-
 import copy
 
 #- Definir el stock de un producto en una variable.
-stock = [[1, 100], [2, 100], [3, 100], [4, 100], [5, 100], [6, 100], [7, 100], [8, 100], [9, 100], [10, 100]]
+stock = {1: 100, 2: 100, 3: 100, 4: 100, 5: 100, 6: 100, 7: 100, 8: 100, 9: 100, 10: 100}
 
 
 #- Definir una forma de solicitar el stock disponible del producto por consola.
-def mostrar_stock(codigo):
-    print(f"El producto {codigo} tiene un stock de {stock[codigo-1][1]} unidades")
+
+def consulta_stock(codigo_stock):
+    print(f"El producto {codigo_stock} tiene un stock de {stock[codigo_stock]} unidades")
    
 
 #- Definir una forma de solicitar unidades del producto por consola. Este número de productos se
 #almacenarán en una nueva variable llamada ‘Productos seleccionados’.
-productos_seleccionados_plantilla = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0]]
+productos_seleccionados_plantilla = dict()
 productos_seleccionados = copy.deepcopy(productos_seleccionados_plantilla)
 
-def pedir(codigo, cantidad):# corregir: agregar verificación de stock
+def pedir(codigo, cantidad):
     global productos_seleccionados #para usar el scope global de la variable
-    productos_seleccionados[(codigo-1)][1] = cantidad #corregir: lista de sólo los itemes seleccionados
+    if cantidad > 20: # si pide más de 20
+        print(f"No se pueden pedir más de 20 unidades. Usted pidió {cantidad}")
+    elif (cantidad +1) <= stock[codigo] and cantidad >= 12: #si pide 12 o más, va uno de yapa
+        productos_seleccionados.update({codigo: (cantidad + 1)}) #se agrega con yapa
+        stock[codigo] -= cantidad+1
+        print(f"Producto {codigo} : Se agregaron {cantidad} más 1 unidad, de yapa al pedido, {cantidad + 1} en total.")
+    elif cantidad <= stock[codigo]: #si son menos de 12
+        productos_seleccionados[codigo] = cantidad 
+        stock[codigo] -= cantidad
+        print(f"Producto {codigo}: Se agregaron {cantidad} unidades al pedido")
+    else: #si no queda stock  
+        print(f"No quedan suficientes unidades del Item {codigo}") 
+    
+
+    #productos_seleccionados[(codigo-1)][1] = cantidad #corregir: lista de sólo los itemes seleccionados
      
     
 
 #- Los productos reubicados serán descontados del stock inicial.
-
+"""
 for item in productos_seleccionados:
     if ((stock[item[0]-1][1]) - (productos_seleccionados[item[0]-1][1])) < 0: ##- El programa debe verificar que existan unidades disponibles.
         print(f"No hay suficiente stock del producto código N° {item[0]}")
     else:
-        stock[item[0]-1][1] = (stock[item[0]-1][1]) - (productos_seleccionados[item[0]-1][1]) #si hay stock, descuenta
+        stock[item[0]-1][1] = (stock[item[0]-1][1]) - (productos_seleccionados[item[0]-1 hay stock, descuenta
        
-#borrar carrito
-def borrar_carrito():       
-    global productos_seleccionados
-    productos_seleccionados = []
+#borrar carrito       
+productos_seleccionados = copy.deepcopy(productos_seleccionados_plantilla)    
+    
   
 
-
-
-
 #- Al verificar las unidades disponibles, el programa entregará una unidad extra cuando se seleccionen
-#más de 12 unidades. Verificar que el stock posibilite entregar una unidad extra. Sino se entregan las
-#unidades justas. Cada una de las posibles acciones debe imprimir un mensaje explicando lo realizado.
-
-
-
-#- No se pueden solicitar más de 20 unidades en un mismo pedido.
-
-
-
-#- Si el valor ingresado es superior al stock disponible, este debe entregar un mensaje indicando que no
+# más de 12 unidades. Verificar que el stock posibilite entregar una unidad extra. Sino se entregan las
+# unidades justas. Cada una de las posibles acciones debe imprimir un mensaje explicando lo realizado.
+# - No se pueden solicitar más de 20 unidades en un mismo pedido.
+# - Si el valor ingresado es superior al stock disponible, este debe entregar un mensaje indicando que no
 #es posible realizar esta acción debido a que no hay stock suficiente.
+"""
